2af491e6f353c7366d0f6d16e1e62c2fa3f048ea
Rajeev vishwakarma <rvishwakarma00786@gmail.com>
2020-09-20 16:21:18 +0530

    heavy changes 01

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+var spanText = function spanText(text) {
+  var string = text.innerText;
+  var spaned = '';
+  for (var i = 0; i < string.length; i++) {
+    if(string.substring(i, i + 1) === ' ') spaned += string.substring(i, i + 1);
+    else spaned += '<span>' + string.substring(i, i + 1) + '</span>';
+  }
+  text.innerHTML = spaned;
+}
+
+
+var headline = document.querySelector("h1");
+
+spanText(headline);
